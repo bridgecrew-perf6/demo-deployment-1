a565d80f506068d7713b63678c02931f91b9dd79
Elliott Leonard <egleonard88@gmail.com>
2022-04-27 14:04:29 -0600

    changes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, '../index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-const port = process.env.PORT || 4005
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "../style.css"));
+});
+
+const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
